e7999d2689c96187ea0821c8c242554f1524ae75
Afonso Haruo Carnielli Mukai <afonso.mukai@lnls.br>
2015-04-29 15:23:28 -0300

    Add code to initial tests

--- model.py
+++ model.py
@@ -10,7 +10,8 @@ the server.
 import time
 import queue
 import pyaccel
-import sirius.SI_V07
+import sirius
+import va.pv_names as pv_names
 
 
 PROCESSING_INTERVAL = 0.05
@@ -21,12 +22,22 @@ class Model(object):
     def __init__(self, machine):
         self._is_processing = False
         self._accelerator = machine.create_accelerator()
+        self._accelerator[10].hkick_polynom = 1.0e-4
         self._queue = queue.Queue()
 
-    def get_pv(self, pv):
+    def get_pv(self, reason):
+        if 'BPM' in reason:
+            self._calc_orbit()
+            orbit = self._orbit[:, pv_names.bpm[reason[2:]]]
+            return (orbit[0], orbit[2])
+
+        # if pv == 'PS':
+        #     return self.pvs[pv]
+        # elif pv == 'BPM':
+        #     return 10*self.pvs['PS'] + 0.1
         # Identify PV
         # Return PV value
-        return 0.0
+        # return (0.0, 1.0)
 
     def set_pv(self, pv, value):
         # Validate input
@@ -46,6 +57,10 @@ class Model(object):
         while(self._is_processing):
             while not self._queue.empty():
                 value = self._queue.get()
+
+                # name, value = value
+                # self.pvs[name] = value
+
                 print(value)
             time.sleep(PROCESSING_INTERVAL)
         print('stopping processing loop')
@@ -53,8 +68,11 @@ class Model(object):
     def stop(self):
         self._is_processing = False
 
+    def _calc_orbit(self):
+        self._orbit = pyaccel.tracking.findorbit4(self._accelerator, indices='open')
+
 
 class SiModel(Model):
 
     def __init__(self):
-        super().__init__(sirius.SI_V07)
+        super().__init__(sirius.si)
--- server.py
+++ server.py
@@ -2,7 +2,32 @@
 import time
 import signal
 import threading
-import va.model as model
+from pcaspy import Driver, SimpleServer
+import va.model as models
+import va.pv_names as pv_names
+
+
+class PCASDriver(Driver):
+    def  __init__(self, si_model):
+        super().__init__()
+        self.si_model = si_model
+
+    def read(self, reason):
+        print(reason)
+        if reason.startswith('SI'):
+            value = self.si_model.get_pv(reason)
+        else:
+            raise Exception('model not implemented')
+
+        return value
+
+    def write(self, reason, value):
+        if reason.startswith('SI'):
+            model = self.si_model
+        else:
+            raise Exception('model not implemented')
+
+        model.set_pv(reason, value)
 
 
 class ModelThread(threading.Thread):
@@ -21,22 +46,32 @@ class ModelThread(threading.Thread):
 
 
 def handler(signum, frame):
-    global t
+    global si_thread
+    global is_running
     print('Received signal', signum)
     print('Active thread count:', threading.active_count())
-    print('Is alive?', t.stop())
+    print('Is alive?', si_thread.stop())
+    is_running = False
 
 
 if __name__ == '__main__':
 
     signal.signal(signal.SIGINT, handler)
 
-    si = model.SiModel()
+    si = models.SiModel()
 
-    t = ModelThread(si)
-    t.start()
+    si_thread = ModelThread(si)
+    si_thread.start()
 
-    si.set_pv('A1', 1.0)
-    si.set_pv('A2', 2.1)
-    time.sleep(0.5)
-    si._queue.put('B')
+
+    pvdb = {}
+    for key in pv_names.bpm.keys():
+        pvdb['SI'+key] = {'type' : 'float', 'count': 2}
+
+    server = SimpleServer()
+    server.createPV('', pvdb)
+    driver = PCASDriver(si)
+
+    is_running = True
+    while is_running:
+        server.process(0.1)
