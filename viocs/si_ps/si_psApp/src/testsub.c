b5eac597907e18d26157be45a50a85dbe6cfc857
Afonso Haruo Carnielli Mukai <afonso.mukai@lnls.br>
2015-05-08 16:21:09 -0300

    Add interpolation and extrapolation to PS IOC

--- viocs/si_ps/si_psApp/src/testsub.c
+++ viocs/si_ps/si_psApp/src/testsub.c
@@ -12,12 +12,16 @@
 int num_pts;
 double xt[MAXTABSIZE], yt[MAXTABSIZE];
 
-double interp(double x);
+
+double calculate(double x);
+double interpolate(double x);
+double extrapolate(double x);
 
 long testSubInit(struct subRecord *psub)
 {
     printf("init was called\n");
     printf("A: %7.3f\n", psub->a);
+    printf("Name: %s\n", psub->name);
 
     num_pts = 20;
     int i;
@@ -36,27 +40,55 @@ long testSubProcess(struct subRecord *psub)
     printf("process was called\n");
     printf("A: %7.3f\n", psub->a);
 
-    psub->b = interp(psub->a);
+    psub->b = interpolate(psub->a);
 
     return 0;
 }
 
-double interp(double x)
+double calculate(double x)
+{
+    if (x>=xt[0] && x<xt[num_pts-1])
+        return interpolate(x);
+    else
+        return extrapolate(x);
+}
+
+double interpolate(double x)
 {
     int i;
+    double xa, xb, ya, yb, dx, dy, y;
 
-    for (i=0; i<num_pts; ++i)
+    for (i=0; i<num_pts-1; ++i)
         if (x < xt[i+1])
             break;
 
-    double xa = xt[i];
-    double xb = xt[i+1];
-    double ya = yt[i];
-    double yb = yt[i+1];
-    double dx = xb - xa;
-    double dy = yb - ya;
+    xa = xt[i];
+    xb = xt[i+1];
+    ya = yt[i];
+    yb = yt[i+1];
+    dx = xb - xa;
+    dy = yb - ya;
 
-    return ya + (x - xa)*dy/dx;
+    y = ya + (x - xa)*dy/dx;
+
+    return y;
+}
+
+double extrapolate(double x)
+{
+    double dx, dy, y;
+
+    if (x < xt[0]) {
+        dx = xt[1] - xt[0];
+        dy = yt[1] - yt[0];
+        y = yt[0] + (x - xt[0])*dy/dx;
+    } else {
+        dx = xt[num_pts-1] - xt[num_pts-2];
+        dy = yt[num_pts-1] - yt[num_pts-2];
+        y = yt[num_pts-1] + (x - xt[num_pts-1])*dy/dx;
+    }
+
+    return y;
 }
 
 epicsRegisterFunction(testSubInit);
