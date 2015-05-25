aa33f32c5ef7ec8fc874f96bbca90e19e528d39b
ximenes <xresende@gmail.com>
2015-05-25 17:00:12 -0300

    Clean code in va.sh

--- va.sh
+++ va.sh
@@ -1,11 +1,29 @@
 #!/bin/bash
 
 ### This script is supposed to be run at lnls82-linux
+### It starts all virtual IOCs using procServ
 
 export EPICS_CA_AUTO_ADDR_LIST=NO
 export EPICS_CA_ADDR_LIST=10.0.21.255
+VIOCS=$FACCODE/va/viocs
+port=23000
+declare -a viocsprojs=("si_bpms" "si_current" "si_lifetime" "si_ps" "si_tune")
 
-procServ -n vaca -i ^D^C 23000 $FACCODE/va/server.py VA-
-procServ -n si_current -i ^D^C 23001 $FACCODE/va/viocs/si_current/si_currentApp/src/O.linux-x86_64/si_current $FACCODE/va/viocs/si_current/iocBoot/iocsi_current/st.cmd
+
+### guarantees that IOCs are executable by anyone
+chmod a+x $FACCODE/va/server.py
+for ioc in "${viocsprojs[@]}"
+do
+    chmod a+x $VIOCS/$ioc/$ioc"App"/src/O.linux-x86_64/$ioc
+done
+
+### starts VIOCS
+procServ -n server -i ^D^C $port $FACCODE/va/server.py VA-
+for ioc in "${viocsprojs[@]}"
+do
+    port=$((port+1))
+    #echo $ioc" "$port
+    procServ -n $ioc -i ^D^C $port $VIOCS/$ioc/$ioc"App"/src/O.linux-x86_64/$ioc $VIOCS/$ioc/iocBoot/"ioc"$ioc/st.cmd
+done
 
 
--- vaca.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-### This script is supposed to be run at lnls82-linux
-
-export EPICS_CA_AUTO_ADDR_LIST=NO
-export EPICS_CA_ADDR_LIST=10.0.21.255
-
-procServ -n server -i ^D^C 23000 $FAC_CODE/va/server.py VA-
-
