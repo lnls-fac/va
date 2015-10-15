dd62b171eca5106da0c348895c88ef06889bb1ee
Afonso Haruo Carnielli Mukai <afonso.mukai@lnls.br>
2015-10-15 13:36:21 -0300

    Add skew fields to excitation_curve.py and tests

--- test/test_excitation_curve.py
+++ test/test_excitation_curve.py
@@ -15,7 +15,7 @@ class TestInitException(unittest.TestCase):
         with self.assertRaises(FileNotFoundError):
             curve = ExcitationCurve(file_name)
 
-    def test_init_not_increasing_tables(self):
+    def test_init_not_increasing(self):
         file_name = os.path.join(EXC_CURVE_DIR, 'bad_test.txt')
         with self.assertRaises(ValueError):
             curve = ExcitationCurve(file_name)
@@ -42,30 +42,124 @@ class TestExcitationCurve(unittest.TestCase):
         with self.assertRaises(AttributeError):
             self.curve.harmonics = [0, 1, 2, 3]
 
-    def test_get_main_field(self):
+    def test_get_normal_main_field(self):
+        # First current interval
         current = 25.2
         # Use values from excitation curve
         expected_main_field = (current/50.0)*(-0.4)
-        main_field = self.curve.get_main_field_from_current(current)
+        main_field = self.curve.get_normal_main_field_from_current(current)
         self.assertAlmostEqual(main_field, expected_main_field, 8)
 
+        # Second current interval
         current = 55.1
         # Use values from excitation curve
-        expected_main_field = -0.4 + (current-50.0)/(50.0)*(-0.6)
-        main_field = self.curve.get_main_field_from_current(current)
+        expected_main_field = -0.4 + (current-50.0)/50.0*(-0.6)
+        main_field = self.curve.get_normal_main_field_from_current(current)
         self.assertAlmostEqual(main_field, expected_main_field, 8)
 
-    def test_get_current(self):
+    def test_get_skew_main_field(self):
+        # First current interval
+        current = 25.2
+        # Use values from excitation curve
+        expected_main_field = (current/50.0)*0.8
+        main_field = self.curve.get_skew_main_field_from_current(current)
+        self.assertAlmostEqual(main_field, expected_main_field, 8)
+
+        # Second current interval
+        current = 55.1
+        # Use values from excitation curve
+        expected_main_field = 0.8 + (current-50.0)/(50.0)*1.2
+        main_field = self.curve.get_skew_main_field_from_current(current)
+        self.assertAlmostEqual(main_field, expected_main_field, 8)
+
+    def test_get_normal_fields(self):
+        # First current interval
+        current = 30.3
+        # Use values from excitation curve
+        fields_t1 = numpy.zeros(14)
+        fields_t1[1] = -0.4
+        fields_t1[4] = -1.2
+        fields_t1[9] = -2.0
+        fields_t1[13] = -2.8
+
+        expected_fields = (current/50.0)*fields_t1
+        fields = self.curve.get_normal_fields_from_current(current)
+        self.assertEqual(len(fields), 14)
+        for i in range(14):
+            self.assertAlmostEqual(fields[i], expected_fields[i], 8)
+
+        # Second current interval
+        current = 70.9
+        # Use values from excitation curve
+        fields_t2 = numpy.zeros(14)
+        fields_t2[1] = -1.0
+        fields_t2[4] = -3.0
+        fields_t2[9] = -5.0
+        fields_t2[13] = -7.0
+
+        expected_fields = fields_t1 + (current-50.0)/50.0*(fields_t2-fields_t1)
+        fields = self.curve.get_normal_fields_from_current(current)
+        for i in range(14):
+            self.assertAlmostEqual(fields[i], expected_fields[i], 8)
+
+    def test_get_skew_fields(self):
+        # First current interval
+        current = 22.1
+        # Use values from excitation curve
+        fields_t1 = numpy.zeros(14)
+        fields_t1[1] = 0.8
+        fields_t1[4] = 1.6
+        fields_t1[9] = 2.4
+        fields_t1[13] = 3.2
+
+        expected_fields = (current/50.0)*fields_t1
+        fields = self.curve.get_skew_fields_from_current(current)
+        self.assertEqual(len(fields), 14)
+        for i in range(14):
+            self.assertAlmostEqual(fields[i], expected_fields[i], 8)
+
+        # Second current interval
+        current = 80.8
+        # Use values from excitation curve
+        fields_t2 = numpy.zeros(14)
+        fields_t2[1] = 2.0
+        fields_t2[4] = 4.0
+        fields_t2[9] = 6.0
+        fields_t2[13] = 8.0
+
+        expected_fields = fields_t1 + (current-50.0)/50.0*(fields_t2-fields_t1)
+        fields = self.curve.get_skew_fields_from_current(current)
+        for i in range(14):
+            self.assertAlmostEqual(fields[i], expected_fields[i], 8)
+
+    def test_get_current_from_normal(self):
+        # First field interval
         main_field = -0.23
         # Use values from excitation curve
         expected_current = (-main_field/0.4)*50.0
-        current = self.curve.get_current_from_main_field(main_field)
+        current = self.curve.get_current_from_normal_main_field(main_field)
         self.assertAlmostEqual(current, expected_current, 8)
 
+        # Second field interval
         main_field = -0.77
         # Use values from excitation curve
         expected_current = 50.0 + (main_field+0.4)/(-0.6)*50.0
-        current = self.curve.get_current_from_main_field(main_field)
+        current = self.curve.get_current_from_normal_main_field(main_field)
+        self.assertAlmostEqual(current, expected_current, 8)
+
+    def test_get_current_from_skew(self):
+        # First field interval
+        main_field = 0.46
+        # Use values from excitation curve
+        expected_current = (main_field/0.8)*50.0
+        current = self.curve.get_current_from_skew_main_field(main_field)
+        self.assertAlmostEqual(current, expected_current, 8)
+
+        # Second field interval
+        main_field = 1.54
+        # Use values from excitation curve
+        expected_current = 50.0 + (main_field-0.8)/1.2*50.0
+        current = self.curve.get_current_from_skew_main_field(main_field)
         self.assertAlmostEqual(current, expected_current, 8)
 
 
--- va/excitation_curve.py
+++ va/excitation_curve.py
@@ -8,6 +8,11 @@ HEADER_CHAR = '#'
 class ExcitationCurve:
 
     def __init__(self, magnet): # generalise: pass magnet name
+        """Conversion between current and field
+
+        Keyword argument:
+        magnet -- magnet excitation curve file name
+        """
         self._load_excitation_curve(magnet)
 
     @property
@@ -18,17 +23,49 @@ class ExcitationCurve:
     def harmonics(self):
         return self._harmonics
 
-    def get_main_field_from_current(self, current):
+    def get_normal_main_field_from_current(self, current):
+        field_array = self._i_to_f_normal_fields[:, self._main_harmonic_index]
+        return self._interpolate_current(current, field_array)
+
+    def get_skew_main_field_from_current(self, current):
+        field_array = self._i_to_f_skew_fields[:, self._main_harmonic_index]
+        return self._interpolate_current(current, field_array)
+
+    def get_normal_fields_from_current(self, current):
+        fields_array = self._i_to_f_normal_fields
+        return self._get_fields_from_current(current, fields_array)
+
+    def get_skew_fields_from_current(self, current):
+        fields_array = self._i_to_f_skew_fields
+        return self._get_fields_from_current(current, fields_array)
+
+    def _get_fields_from_current(self, current, fields_array):
+        fields = _numpy.zeros(self._highest_harmonic+1)
+        for h, i in zip(self._harmonics, range(len(self._harmonics))):
+            fields[h] = self._interpolate_current(current, fields_array[:, i])
+
+        return fields
+
+    def _interpolate_current(self, current, field_array):
         current_array =  self._i_to_f_current
-        main_field_array = self._i_to_f_fields[:, self._main_harmonic_index]
+        return _numpy.interp(current, current_array, field_array)
 
-        return _numpy.interp(current, current_array, main_field_array)
+    def get_current_from_normal_main_field(self, main_field):
+        field_array = self._f_to_i_normal_main_field
+        current_array =  self._f_to_i_normal_current
 
-    def get_current_from_main_field(self, main_field):
-        field_array = self._f_to_i_main_field
-        current_array =  self._f_to_i_current
+        return self._interpolate_main_field(main_field, field_array,
+            current_array)
 
-        return _numpy.interp(main_field, field_array, current_array)
+    def get_current_from_skew_main_field(self, main_field):
+        field_array = self._f_to_i_skew_main_field
+        current_array =  self._f_to_i_skew_current
+
+        return self._interpolate_main_field(main_field, field_array,
+            current_array)
+
+    def _interpolate_main_field(self, field, field_array, current_array):
+        return _numpy.interp(field, field_array, current_array)
 
     def _load_excitation_curve(self, magnet):
         """Generalise in a future version: read from DB?"""
@@ -48,7 +85,6 @@ class ExcitationCurve:
         self._main_harmonic_index = self._harmonics.index(self._main_harmonic)
 
         conversion_data = _numpy.array(conversion_data)
-
         if _numpy.size(conversion_data[:,1:], 1) != 2*(len(self._harmonics)):
             raise Exception('Mismatch between number of columns and size of harmonics list in excitation curve')
 
@@ -76,26 +112,42 @@ class ExcitationCurve:
 
     def _read_harmonics_from_words(self, words):
         self._harmonics = [int(n) for n in words[1:]]
+        self._highest_harmonic = max(self._harmonics)
 
     def _prepare_interpolation_tables(self, current, fields):
         # Interpolation requires increasing x
         if _numpy.all(_numpy.diff(current) > 0):
             self._i_to_f_current = current
-            self._i_to_f_fields = fields
+            self._i_to_f_normal_fields = fields[:, 0::2]
+            self._i_to_f_skew_fields = fields[:, 1::2]
         elif _numpy.all(_numpy.diff(current) < 0):
             self._i_to_f_current = current[::-1]
-            self._i_to_f_fields = fields[::-1, :]
+            self._i_to_f_normal_fields = fields[::-1, 0::2]
+            self._i_to_f_skew_fields = fields[::-1, 1::2]
         else:
             msg = 'current must be strictly increasing or decreasing'
             raise ValueError(msg)
 
-        main_field = fields[:, self._main_harmonic_index]
-        if _numpy.all(_numpy.diff(main_field) > 0):
-            self._f_to_i_current = current
-            self._f_to_i_main_field = main_field
-        elif _numpy.all(_numpy.diff(main_field) < 0):
-            self._f_to_i_current = current[::-1]
-            self._f_to_i_main_field = main_field[::-1]
+        index = 2*self._main_harmonic_index
+
+        normal_field = fields[:, index]
+        if _numpy.all(_numpy.diff(normal_field) > 0):
+            self._f_to_i_normal_current = current
+            self._f_to_i_normal_main_field = normal_field
+        elif _numpy.all(_numpy.diff(normal_field) < 0):
+            self._f_to_i_normal_current = current[::-1]
+            self._f_to_i_normal_main_field = normal_field[::-1]
+        else:
+            msg = 'main field must be strictly increasing or decreasing'
+            raise ValueError(msg)
+
+        skew_field = fields[:, index+1]
+        if _numpy.all(_numpy.diff(skew_field) > 0):
+            self._f_to_i_skew_current = current
+            self._f_to_i_skew_main_field = skew_field
+        elif _numpy.all(_numpy.diff(skew_field) < 0):
+            self._f_to_i_skew_current = current[::-1]
+            self._f_to_i_skew_main_field = skew_field[::-1]
         else:
             msg = 'main field must be strictly increasing or decreasing'
             raise ValueError(msg)
