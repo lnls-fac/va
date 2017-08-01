df375670e249c1876ec4a95c15eea373e94f9838
Fernando <fernandohds564@gmail.com>
2017-08-01 10:37:16 -0300

    Bug fix in sirius-vaca.py script.

--- scripts/sirius-vaca.py
+++ scripts/sirius-vaca.py
@@ -1,14 +1,15 @@
-#!/usr/bin/python3.4
+#!/usr/bin/python3
 
 import sys
 import va.server
 import siriuspy
 
 prefix = siriuspy.envars.vaca_prefix
+only_orbit = False
 if len(sys.argv) > 1:
     only_orbit = sys.argv[1].lower().endswith('true')
 if len(sys.argv) > 2:
     only_orbit = sys.argv[1].lower().endswith('true')
     prefix = sys.argv[2]
 
-va.server.run(prefix,only_orbit=only_orbit)
+va.server.run(prefix, only_orbit=only_orbit)
