b2f7db1d8530c3a935dd2a718167af5ad4edbc3e
Fernando <fernandohds564@gmail.com>
2017-08-17 11:36:46 -0300

    Change import order for speeding help message.

--- scripts/sirius-vaca.py
+++ scripts/sirius-vaca.py
@@ -2,7 +2,6 @@
 
 import argparse as _argparse
 from siriuspy.envars import vaca_prefix as prefix
-import va.server
 
 parser = _argparse.ArgumentParser(description="Run VACA.")
 parser.add_argument('-o', '--orbit', action='store_true', default=False,
@@ -12,4 +11,6 @@ parser.add_argument('-f', '--pvs', action='store_true', default=False,
 parser.add_argument('-p', "--prefix", type=str, default='',
                     help="prefix to be used")
 args = parser.parse_args()
+
+import va.server
 va.server.run(args.prefix or prefix, only_orbit=args.orbit, print_pvs=args.pvs)
