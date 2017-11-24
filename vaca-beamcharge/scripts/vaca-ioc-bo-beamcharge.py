4bea081b2dbfb4b0fa331fa08c997e02dba330b4
Guilherme <guilherme.prado@lnls.br>
2017-11-24 11:10:48 -0200

    Fixing import on vaca bo beam charge ioc script

--- vaca-beamcharge/scripts/vaca-ioc-bo-beamcharge.py
+++ vaca-beamcharge/scripts/vaca-ioc-bo-beamcharge.py
@@ -1,6 +1,6 @@
 #!/usr/local/bin/python-sirius -u
 """Beam Charge Simulation IOC executable."""
 
-from vaca_beamcharge import vaca_beamcharge as ioc_module
+from vaca_beamcharge import driver as ioc_module
 
 ioc_module.run("BO")
