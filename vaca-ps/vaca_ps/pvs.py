fbb23daff78a7de5c23fdc0436356424fef3eec3
ximenes <xresende@gmail.com>
2018-01-08 14:07:17 -0200

    Update vaca-ps in order to use new pwrsupply

--- vaca-ps/vaca_ps/pvs.py
+++ vaca-ps/vaca_ps/pvs.py
@@ -1,12 +1,12 @@
 """Module to create PV database."""
 
 import os as _os
-from siriuspy.pwrsupply_orig import PowerSupplySim as _PowerSupplySim
-from siriuspy.pulsedps.model \
-    import PulsedPowerSupplySim as _PulsedPowerSupplySim
 from siriuspy import envars as _envars
 from siriuspy.search import PSSearch as _PSSearch
 import siriuspy.util as _util
+from siriuspy.pwrsupply.controller import ControllerSim as _ControllerSim
+from siriuspy.pwrsupply.controller import PUControllerSim as _PUControllerSim
+from siriuspy.pwrsupply.model import PowerSupply as _PowerSupply
 
 
 _PREFIX = _envars.vaca_prefix
@@ -29,17 +29,14 @@ def get_ps_devices():
         # Create objects that'll handle the magnets
         print('creating pv database...')
         for ps in pwr_supplies:
-            # if 'PS-QDA' in ps or 'B1B2' in ps or _re.match("SI-\d\w{2}:PS-QDA", ps):
-            #     ps_devices[ps] = _PowerSupplySim(psname=ps)
-            #     #ps_devices[ps] = PS(psname=ps)
             if ps in ps_exc_list:
                 continue
-            if "PU" in ps:
-                ps_devices[ps] = _PulsedPowerSupplySim(psname=ps)
+            if 'PU' in ps:
+                c = _PUControllerSim(psname=ps)
             else:
-                ps_devices[ps] = _PowerSupplySim(psname=ps)
+                c = _ControllerSim(psname=ps)
+            ps_devices[ps] = _PowerSupply(controller=c, psnames=[ps, ])
         print('finished')
-
 
     return ps_devices
 
@@ -52,7 +49,7 @@ def get_pvs_database():
     db = {'AS-Glob:PS-Test:Version-Cte':
           {'type': 'str', 'value': _COMMIT_HASH}}
     for psname in ps_devices:
-        ps_db = ps_devices[psname].database
+        ps_db = ps_devices[psname].get_database()
         props = list(ps_db.keys())
         for i in range(len(props)):
             # if props[i] == 'Current-SP':
