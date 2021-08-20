9016f8b1ae8063ae69a667646e6457e9b7b8c651
ximenes <xresende@gmail.com>
2021-08-20 12:16:18 -0300

    Add script to print lib versions

---
+++ scripts/vaca-check-libs.py
@@ -0,0 +1,27 @@
+#!/usr/local/bin/python-sirius
+
+import va
+import siriuspy
+import mathphys
+import lnls
+import pymodels
+import pyaccel
+import trackcpp
+import siriushla
+import pydm
+import epics
+import pcaspy
+
+print('va        version: {}'.format(va.__version__))
+print('siriuspy  version: {}'.format(siriuspy.__version__))
+print('mathphys  version: {}'.format(mathphys.__version__))
+print('lnls      version: {}'.format(lnls.__version__))
+print('pymodels  version: {}'.format(pymodels.__version__))
+print('pyaccel   version: {}'.format(pyaccel.__version__))
+print('trackcpp  version: {}'.format(trackcpp.__version__))
+print('siriushla version: {}'.format(siriushla.__version__))
+print('pdm       version: {}'.format(pydm.__version__))
+print('pyepics   version: {}'.format(epics.__version__))
+print('pcaspy    version: {}'.format(pcaspy.__version__))
+
+
